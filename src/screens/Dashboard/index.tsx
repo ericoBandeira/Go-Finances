eab826ab8be1722d8eb37bc73aa21d88c103823f
Érico Maximiano Bandeira <ericobandeira@hotmail.com>
2022-06-17 17:19:51 -0300

    Add components

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,6 +1,21 @@
 import React from 'react';
-import { Text, View } from 'react-native';
-import { Container, Header, UserWapper, UserInfo, Photo, User, UserGreeting, UserName} from './styles';
+import { HighlightCard } from '../../components/HighlightCard';
+import { Title } from '../../components/HighlightCard/styles';
+import { TransactionCard } from '../../components/TransactionCard';
+import {
+    Container,
+    Header,
+    UserWapper,
+    UserInfo,
+    Photo,
+    User,
+    UserGreeting,
+    UserName,
+    Icon,
+    HighlightCards,
+    Transactions,
+    TransactionsTitle
+} from './styles';
 
 export function Dashboard() {
     return (
@@ -18,8 +33,39 @@ export function Dashboard() {
                             </UserName>
                         </User>
                     </UserInfo>
+                    <Icon name="power"/>
                 </UserWapper>
             </Header>
+            
+            <HighlightCards >
+                <HighlightCard
+                    type="up"
+                    title="Entradas"
+                    amount="R$ 17.400,00"
+                    lastTransaction="Última entrada dia 13 de abril"
+                />
+                <HighlightCard
+                    type="down"
+                    title="Saídas"
+                    amount="R$ 1.259,00"
+                    lastTransaction="Última saída dia 03 de abril"
+                />
+                <HighlightCard
+                    type="total"
+                    title="Total"
+                    amount="R$ 16.141,00"
+                    lastTransaction="01 à 15 de abril"
+                />
+            </HighlightCards>
+ 
+            <Transactions>
+                <TransactionsTitle>
+                    Listagem
+                </TransactionsTitle>
+
+                <TransactionCard />
+            </Transactions>
+
         </Container>
     )
 }
